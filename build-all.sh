4a6ff51e64c6bd9756b4e65445f446a926b0ea10
ruffrey <jeff@ruffrey.com>
2021-10-25 11:24:31 -0700

    Release architecture tweaks

--- build-all.sh
+++ build-all.sh
@@ -14,7 +14,7 @@ mkdir out/
 for GOOS in $PLATFORMS; do
   for GOARCH in $ARCHITECTURES; do
     echo $GOOS $GOARCH
-    go build -ldflags="${LDFLAGS}" -o "out/dracula-cli_${GOOS}-${GOARCH}-${VERSION}" cli/main.go
-    go build -ldflags="${LDFLAGS}" -o "out/dracula-server_${GOOS}-${GOARCH}-${VERSION}" cmd/main.go
+    GOOS=$GOOS GOARCH=$GOARCH go build -ldflags="${LDFLAGS}" -o "out/dracula-cli_${GOOS}-${GOARCH}-${VERSION}" cli/main.go
+    GOOS=$GOOS GOARCH=$GOARCH go build -ldflags="${LDFLAGS}" -o "out/dracula-server_${GOOS}-${GOARCH}-${VERSION}" cmd/main.go
   done
 done
